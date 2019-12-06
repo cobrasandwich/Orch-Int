1cdf6459c25ea7c1cb36cd283f77d27af3fd8ecc
cobrasandwich <scanlan.james@yahoo.co.uk>
2019-12-06 00:36:40 +0000

    identify objects via hover and seperately make them clickable

--- OrcInt/Assets/Scripts/ClickManager.cs
+++ OrcInt/Assets/Scripts/ClickManager.cs
@@ -1,29 +1,46 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ClickManager : MonoBehaviour
 {
-    //public Camera camera;
-    RaycastHit hit;
-    Ray ray;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject hoveredObject;
 
     // Update is called once per frame
     void Update()
     {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
 
-        if (Input.GetMouseButtonDown(0))
-        {
-            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out hit))
-            {
-                Debug.Log(hit.collider.gameObject.name);
+        if (Physics.Raycast(ray, out hit)) {
+
+            if (Input.GetMouseButtonDown(0)) {
+                Debug.Log("MOUSE PRESSED " + hit.collider.gameObject.name);
             }
+
+            GameObject hitObject = hit.collider.gameObject;
+            SelectObject(hitObject);
+            Debug.Log(hoveredObject);
         }
+        else {
+            ClearSelection();
+        }
+    }
+
+    void SelectObject(GameObject obj) {
+
+        if (hoveredObject != null) {
+            if(obj == hoveredObject) {
+                return;
+            }
+            ClearSelection();
+        }
+
+        hoveredObject = obj;
+    }
+
+    void ClearSelection() {
+        hoveredObject = null;
     }
 }
